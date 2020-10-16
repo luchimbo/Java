c92c8c7a24c9c313386c429ce10fe7f22250a79a
luchimbo <luciotambo@gmail.com>
2020-10-16 10:54:44 -0300

    Ejercicio

---
+++ PrimerPrograma/src/primerprograma/PrimerPrograma.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package primerprograma;
+
+public class PrimerPrograma {
+    
+    
+    public static void main(String[] args) {
+        
+        ClasePrueba objeto = new ClasePrueba();
+        objeto.setNumero(5);
+        
+        System.out.println(objeto.getNumero());
+        
+        
+        
+    }
+    
+}
