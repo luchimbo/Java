83a08515a354bdf4be383781697a8cda5d785878
luchimbo <luciotambo@gmail.com>
2020-10-16 10:55:02 -0300

    Ejercicio Clase

---
+++ PrimerPrograma/src/primerprograma/ClasePrueba.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package primerprograma;
+    
+    
+/**
+ *
+ * @author tambo
+ */
+public class ClasePrueba {
+    private int numero;
+    private int numero2;
+    
+    public int getNumero(){
+        return numero;
+    }
+    
+    public void setNumero(int numero){
+        this.numero = numero;
+    }
+    
+    public int getNumero2(){
+        return numero2;
+    }
+    
+    public void setNumero2(int numero2){
+        this.numero2 = numero2;
+    }
+}
